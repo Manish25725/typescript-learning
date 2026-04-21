23d0815749a402f0b4eeff869510dfcd21e5c37e
refactorman <149008521+Manish25725@users.noreply.github.com>
2026-04-21 18:01:28 +0530

    Add role-based redirection and error handling functions in moreTypes.ts; create interface.ts

--- src/moreTypes.ts
+++ src/moreTypes.ts
@@ -14,7 +14,6 @@ type Book = {
 
 
 let bookString ='{name : "who moved my cheese"}';
-
 let bookObject = JSON.parse(bookString) as Book;
 console.log(bookObject);
 
@@ -58,3 +57,37 @@ catch(error){
 
 const data:unknown = "chai aur code";
 const strData : string = data as string;
+
+
+type Role= "admin" | "user" | "superadmin";
+
+//never is used in exhaustive checking to ensure all union type cases are handled, and if nothing is left, the variable becomes never.”
+
+//never means no possible value is left...
+function redirectBasedOnRole(role:Role): void{
+
+    if(role === "admin"){
+        console.log("Redirecting to admin dashboard");
+        return;
+    }
+
+    if(role === "user"){
+        console.log("Redirecting to user dashboard");
+        return;
+    }
+
+    role;
+}
+
+//never is used for functions that throw errors, run infinitely, or for exhaustive checking in union types where no possible value remains.
+
+//never is used when execution never reaches a normal return point.
+
+function neverReturn(): never{
+    while(true){};
+};
+
+
+function throwError(message: string): never {
+    throw new Error(message);
+}
