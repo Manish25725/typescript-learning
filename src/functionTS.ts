2e34aefd96d1383a62d9efb4561fb08ce877e50a
refactorman <149008521+Manish25725@users.noreply.github.com>
2026-04-23 16:32:05 +0530

    Add makeChai, getChaiPrice, and makeOrder functions to functionTS.ts

---
+++ src/functionTS.ts
@@ -0,0 +1,15 @@
+function makeChai(type:string,cups:number){
+    console.log(`Make ${cups} of ${type}`)
+}
+
+makeChai("Masala",3);
+
+
+function getChaiPrice():number{
+    return 12;
+}
+
+function makeOrder(order:string){
+    if(!order) return null;
+    return order;
+}
